87ed06c53a453904bf40b61518f7145e403d8d19
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-10-31 16:03:41 -0300

    feat: make header component return home on click

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,8 +1,17 @@
+import React from 'react';
 import styles from './style.module.scss'
+import { useNavigate } from "react-router-dom";
 
 export default function Header(){
+
+    let navigate = useNavigate(); 
+    const redirectHome  = () => {  
+        let path = "/"; 
+        navigate(path);
+    }
+
     return(
-        <div className={styles.container}>
+        <div className={styles.container} onClick={redirectHome}>
             <h1 className={styles.title}>Green Shopping</h1>
             <img className={styles.logo} src="./leaf.png" alt="Leaf icon" />
         </div>
