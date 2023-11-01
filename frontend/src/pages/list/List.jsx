599f5d069d88df182a066c25fae105002cce1e28
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-11-01 17:40:47 -0300

    feat: add user container to list page

--- frontend/src/pages/list/List.jsx
+++ frontend/src/pages/list/List.jsx
@@ -5,11 +5,30 @@ import InviteIcon from "../../components/invite-icon/InviteIcon";
 export default function List(){
 
     return(
-        <div className="container">
-            <UserIcon username="Adilson"/>
-            <UserIcon username="Guilherme"/>
-            <UserIcon username="Elisa"/>
-            <InviteIcon/>
+        <div className={styles.main}>
+
+            <div className={styles.content}>
+
+                <div className={styles.userContainer}>
+
+                    <div className={styles.icons}>
+                        <UserIcon username="Adilson"/>
+                        <UserIcon username="Guilherme"/>
+                        <UserIcon username="Elisa"/>
+                    </div>
+                    
+                    <InviteIcon/>
+                </div>
+
+                <div className={styles.todocontainer}>
+
+                </div>
+
+            </div>
+            
+
         </div>
+
+
     )
 }
